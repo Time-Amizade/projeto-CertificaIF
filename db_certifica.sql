61917435bd4e814cef15747849468f9fc7d447f1
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2025-05-29 10:45:15 -0300

    Update db_certifica.sql

--- db_certifica.sql
+++ db_certifica.sql
@@ -91,3 +91,9 @@ CREATE TABLE IF NOT EXISTS `Comprovante` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+
+
+INSERT INTO Usuario (nomeUsuario,anoNascimento, cpf, senha, email,telefone,endereco,funcao,status) 
+  VALUES ('admin',01/07/1980, '000.000.000-00', 'senha123', 'admin@gmail.com','(11) 90000-0000','Rua Exemplo, 123 - Cidade - Estado','ADMINISTRADOR','ATIVO');
+
