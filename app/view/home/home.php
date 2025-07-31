16f09e44e0267f9e3ef1ce47b94fd0f5574022e3
Erik Dias Paulinio <116355146+Dev-ErikDias@users.noreply.github.com>
2025-07-31 11:55:51 -0300

    Update home.php

--- app/view/home/home.php
+++ app/view/home/home.php
@@ -3,15 +3,44 @@
 #Objetivo: interface para listagem dos usuários do sistema
 require_once(__DIR__ . "/../include/header.php");
 require_once(__DIR__ . "/../include/menu.php");
+
 ?>
 <link rel="stylesheet" href="<?= BASEURL ?>/view/css/home.css">
 
 <h3 class="text-center">Página inicial do sistema</h3>
-<?php foreach($dados['usuarios'] as $usuario): ?>
-    <p><?= $usuario->getNome() ." ". $usuario->getCodigoMatricula() ?> </p>
-    <br>
-<?php endforeach; ?>
-<a href="<?= BASEURL . '/controller/CursoController.php?action=create'?>">link para</a>
+
+<div class='container'>
+
+  <?php if($_SESSION[SESSAO_USUARIO_PAPEL] == 'ADMINISTRADOR'): ?>
+
+    <?php foreach($dados['usuarios'] as $usuario): ?>
+        <div class='card' style="width: 18rem;">
+          <h5 class='card-title text-center'><?= $usuario->getNome()?></h5>
+          <h6 class='card-subtitle text-center'><?= $usuario->getEmail() ?></h6>
+          <ul class='list-group list-group-flush'>
+            <li class='list-group-item'><?=$usuario->getCursoid()->getNomeCurso()?></li>
+            <li class='list-group-item'><?=$usuario->getCpf()?></li>
+          </ul>
+          <div class='card-footer'>
+            
+          </div>
+        </div>
+        <br>
+    <?php endforeach; ?>
+
+  <?php elseif($_SESSION[SESSAO_USUARIO_PAPEL] == 'COORDENADOR'): ?>
+
+    <?php foreach($dados['usuarios'] as $usuario): ?>
+        <p><?= "O usuário : " . $usuario->getNome() .", tem o seguinte código de matrícula: ". $usuario->getCodigoMatricula() ?> </p>
+        <br>
+    <?php endforeach; ?>
+
+  <?php else: ?>
+    <p>Lista de comprovante</p>
+  <?php endif; ?>
+
+</div>
+  <a href="<?= BASEURL . '/controller/CursoController.php?action=create'?>">link para</a>
     
 <script src="<?= BASEURL ?>/view/js/home_ajax.js"></script>
 
