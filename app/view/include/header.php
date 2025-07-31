6bea5559ea1cc73011cc16de3960d8ac55602102
Erik Dias Paulinio <116355146+Dev-ErikDias@users.noreply.github.com>
2025-07-31 11:56:56 -0300

    Update header.php

--- app/view/include/header.php
+++ app/view/include/header.php
@@ -3,6 +3,7 @@
 #Objetivo: header a ser utilizados em todas as páginas do projeto
 
 include_once(__DIR__ . "/../../util/config.php");
+
 ?>
 <!DOCTYPE html>
 <html lang="pt">
@@ -13,6 +14,8 @@ include_once(__DIR__ . "/../../util/config.php");
     
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="../../../view/css/login.css"> !-->
+    <link rel="stylesheet" href="<?= BASEURL . '/view/css/custom.css'?>"> 
+
 </head>
-<body>
+
+<body class = "<?= $pagina ?>">
