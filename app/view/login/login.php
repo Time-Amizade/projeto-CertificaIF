bcfa5876027c361890c0fba6f524285620ef8448
Vinicius Gonçalves da Paz <vinicius.goncalves.dapaz.tds.2022@gmail.com>
2025-08-06 11:52:57 -0300

    css_login

--- app/view/login/login.php
+++ app/view/login/login.php
@@ -5,37 +5,41 @@ $pagina = 'login';
 
 require_once(__DIR__ . "/../include/header.php");
 ?>
+<div class="container d-flex justify-content-center" style="padding-top: 80px;">
+    <div class="row w-100" style="max-width: 1000px;">
+        <!-- CARD DO FORMULÁRIO -->
+        <div class="col-md-6">
+            <div class="card shadow p-4 rounded" style="width: 700px; background-color: #f8f9fa;">
 
-<div class="container">
-    <div class="row" style="margin-top: 20px; ">
-        <div class="col-6">
-            <div class="alert alert-info">
-                <h4>Informe os dados para logar:</h4>
-                <br>
+                <img src="<?= BASEURL . '/view/img/anexo.png'?>" style="display: block; margin: 0 auto 10px auto; max-width: 500px%; height: auto;">
 
-                <!-- Formulário de login -->
-                <form id="frmLogin" action="./LoginController.php?action=logon" method="POST" >
+                <h4 style="text-align: center;">login:</h4>
+
+                <form id="frmLogin" action="./LoginController.php?action=logon" method="POST">
                     <div class="mb-3">
                         <label class="form-label" for="txtEmail">Login:</label>
                         <input type="text" class="form-control" name="email" id="txtEmail"
-                            maxlength="15" placeholder="Informe o email"
-                            value="<?php echo isset($dados['email']) ? $dados['email'] : '' ?>" />        
+                               maxlength="15" placeholder="Informe o email"
+                               value="<?php echo isset($dados['email']) ? $dados['email'] : '' ?>" />        
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label" for="txtSenha">Senha:</label>
                         <input type="password" class="form-control" name="senha" id="txtSenha"
-                            maxlength="15" placeholder="Informe a senha"
-                            value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>" />        
+                               maxlength="15" placeholder="Informe a senha"
+                               value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>" />        
                     </div>
 
                     <button type="submit" class="btn btn-success mt-3">Acessar</button>
-                    <button  class="btn btn-succes mt-3"><a href="<?= BASEURL . '/controller/CadastroController.php?action=create'?>">Cadastrar</a></button>
+                    <button class="btn btn-success mt-3">
+                        <a href="<?= BASEURL . '/controller/CadastroController.php?action=create' ?>" style="color: white; text-decoration: none;">Cadastrar</a>
+                    </button>
                 </form>
             </div>
         </div>
 
-        <div class="col-6">
+        <!-- MENSAGENS DE ERRO AO LADO -->
+        <div class="col-md-6 d-flex align-items-center">
             <?php include_once(__DIR__ . "/../include/msg.php") ?>
         </div>
     </div>
