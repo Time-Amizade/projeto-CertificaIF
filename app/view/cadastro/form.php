a3e2b3de4543b847e35480e1387687d364e61404
Vinicius Gonçalves da Paz <vinicius.goncalves.dapaz.tds.2022@gmail.com>
2025-08-07 11:49:37 -0300

    alteração_cadastro

--- app/view/cadastro/form.php
+++ app/view/cadastro/form.php
@@ -1,119 +1,128 @@
 <?php
 #Nome do arquivo: usuario/list.php
 #Objetivo: interface para listagem dos usuários do sistema
-
+$pagina = 'cadastro';
 require_once(__DIR__ . "/../include/header.php");
-require_once(__DIR__ . "/../include/menu.php");
+// require_once(__DIR__ . "/../include/menu.php");
 
 ?>
 
-<h3 class="text-center">
-    Cadastrar
-</h3>
 
-<div class="container">
-    <div class="row" style="margin-top: 10px;">
+<div class="container  d-flex justify-content-center" >
+    <div class="row w-200" style="margin-top: 20px;">
         
-        <div class="col-6">
-            <form id="frmUsuario" method="POST" 
-                action="<?= BASEURL ?>/controller/CadastroController.php?action=save" >
-                <div class="mb-3">
-                    <label class="form-label" for="txtNome">Nome Completo:</label>
-                    <input class="form-control" type="text" id="txtNome" name="nome" 
-                        maxlength="70" placeholder="Informe o nome"
-                        value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getNome() : ''); ?>" />
-                </div>
-                
-                <div class="mb-3">
-                    <label class="form-label" for="txtData">Data de Nascimento:</label>
-                    <input class="form-control" type="date" id="txtData" name="dataNascimento" 
-                        value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getDataNascimento() : ''); ?>">
-                </div>
+        <!-- card do formulario-->
+        <div class="card shadow p-2 rounded-lg " style=" width: 800px; height: auto; background-color: rgba(255,255,255,0.7); backdrop-filter: blur(10px); -webkit-backdrop-filter: blur(10px); border: 1px solid rgba(255,255,255,0.3);">
+            <h3 class="text-center">
+                <img src="<?= BASEURL . '/view/img/anexo.png'?>" style="display: block; margin: 0 auto 10px auto; max-width: 500px; height: 300px;">
+            </h3>
+            <div class=>
+                <form id="frmUsuario" method="POST" 
+                    action="<?= BASEURL ?>/controller/CadastroController.php?action=save" >
+                    <div class="mb-3">
+                        <label class="form-label" for="txtNome">Nome Completo:</label>
+                        <input class="form-control" type="text" id="txtNome" name="nome" 
+                            maxlength="70" placeholder="Informe o nome"
+                            value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getNome() : ''); ?>" />
+                    </div>
+                    
+                    <div class="mb-3">
+                        <label class="form-label" for="txtData">Data de Nascimento:</label>
+                        <input class="form-control" type="date" id="txtData" name="dataNascimento" 
+                            value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getDataNascimento() : ''); ?>">
+                    </div>
+                    
+                    <div class="mb-3">
+                        <label  class="form-label" for="cpf">CPF:</label>
+                        <input class="form-control" type="number" id="cpf" name="cpf" 
+                            maxlength="14" placeholder="Informe número de cpf"
+                            value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getCpf() : ''); ?>">
+                    </div>
 
-                <div class="mb-3">
-                    <label class="form-label" for="txtEmail">Email:</label>
-                    <input class="form-control" type="text" id="txtEmail" name="email" 
-                        maxlength="45" placeholder="Informe o Email"
-                        value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getEmail() : ''); ?>"/>
-                </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="txtEmail">Email:</label>
+                        <input class="form-control" type="text" id="txtEmail" name="email" 
+                            maxlength="45" placeholder="Informe o Email"
+                            value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getEmail() : ''); ?>"/>
+                    </div>
 
-                <div class="mb-3">
-                    <label class="form-label" for="txtPassword">Senha:</label>
-                    <input class="form-control" type="password" id="txtPassword" name="senha" 
-                        maxlength="45" placeholder="Informe a senha"
-                        value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getSenha() : ''); ?>"/>
-                </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="txtPassword">Senha:</label>
+                        <input class="form-control" type="password" id="txtPassword" name="senha" 
+                            maxlength="45" placeholder="Informe a senha"
+                            value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getSenha() : ''); ?>"/>
+                    </div>
 
-                <div class="mb-3">
-                    <label class="form-label" for="txtConfSenha">Confirmação da senha:</label>
-                    <input class="form-control" type="password" id="txtConfSenha" name="conf_senha" 
-                        maxlength="45" placeholder="Informe a confirmação da senha"
-                        value="<?php echo isset($dados['confSenha']) ? $dados['confSenha'] : '';?>"/>
-                </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="txtConfSenha">Confirmação da senha:</label>
+                        <input class="form-control" type="password" id="txtConfSenha" name="conf_senha" 
+                            maxlength="45" placeholder="Informe a confirmação da senha"
+                            value="<?php echo isset($dados['confSenha']) ? $dados['confSenha'] : '';?>"/>
+                    </div>
 
-                <div class="mb-3">
-                    <label  class="form-label" for="cpf">Cpf:</label>
-                    <input class="form-control" type="number" id="cpf" name="cpf" 
-                        maxlength="14" placeholder="Informe número de cpf"
-                        value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getCpf() : ''); ?>">
-                </div>
 
-                <div class="mb-3">
-                    <label class="form-label" for="selCurso">Curso:</label>
-                    <select class="form-select" name="curso" id="selCurso">
-                        <option value="">Selecione seu curso</option>
-                        <?php foreach($dados["cursos"] as $curso): ?>
-                            <option value="<?= $curso->getId() ?>" 
-                            <?php 
-                                    if(isset($dados["usuario"]) && $dados["usuario"]->getCursoId() == $curso) 
-                                        echo "selected";
-                                    ?>    
-                            >
-                            <?= $curso->getNomeCurso() ?>
-                        </option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="selCurso">Curso:</label>
+                        <select class="form-select" name="curso" id="selCurso">
+                            <option value="">Selecione seu curso</option>
+                            <?php foreach($dados["cursos"] as $curso): ?>
+                                <option value="<?= $curso->getId() ?>" 
+                                <?php 
+                                        if(isset($dados["usuario"]) && $dados["usuario"]->getCursoId() == $curso) 
+                                            echo "selected";
+                                        ?>    
+                                >
+                                <?= $curso->getNomeCurso() ?>
+                            </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
 
-                
-                <div class="mb-3">
-                    <label class="form-label" for="selfuncao">Função:</label>
-                    <select class="form-select" name="funcao" id="selfuncao">
-                        <option value="">Selecione sua função</option>
-                        <?php foreach($dados["papeis"] as $funcao): ?>
-                            <option value="<?= $funcao ?>" 
-                            <?php 
-                                    if(isset($dados["usuario"]) && $dados["usuario"]->getFuncao() == $funcao) 
-                                        echo "selected";
-                                    ?>    
-                            >
-                            <?= $funcao ?>
-                        </option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-                
-                <div class="mb-3" id="matricula-group" style="display: none;">
-                    <label class="form-label" for="cod_matricula">Código de Matricula</label>
-                    <input class="form-control" type="number" id="cod_matricula" name="cod_matricula" 
-                        placeholder="informe o código de sua matricula"
-                        value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getCodigoMatricula() : ''); ?>">
-                </div>
+                    
+                    <div class="mb-3">
+                        <label class="form-label" for="selfuncao">Função:</label>
+                        <select class="form-select" name="funcao" id="selfuncao">
+                            <option value="">Selecione sua função</option>
+                            <?php foreach($dados["papeis"] as $funcao): ?>
+                                <option value="<?= $funcao ?>" 
+                                <?php 
+                                        if(isset($dados["usuario"]) && $dados["usuario"]->getFuncao() == $funcao) 
+                                            echo "selected";
+                                        ?>    
+                                >
+                                <?= $funcao ?>
+                            </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    
+                    <div class="mb-3" id="matricula-group" style="display: none;">
+                        <label class="form-label" for="cod_matricula">Código de Matricula</label>
+                        <input class="form-control" type="number" id="cod_matricula" name="cod_matricula" 
+                            placeholder="informe o código de sua matricula"
+                            value="<?php echo (isset($dados["usuario"]) ? $dados["usuario"]->getCodigoMatricula() : ''); ?>">
+                    </div>
 
-                <input type="hidden" id="hddId" name="id" 
-                    value="<?= $dados['id']; ?>" />
+                    <input type="hidden" id="hddId" name="id" 
+                        value="<?= $dados['id']; ?>" />
 
-                <div class="mt-3">
-                    <button type="submit" class="btn btn-success">Gravar</button>
-                </div>
-            </form>            
+                    <div class="mt-3">
+                        <button type="submit" class="btn btn-success">Gravar</button>
+                         
+                        <a href="<?= LOGIN_PAGE ?>" class="btn btn-primary mt-3">Cadastrar</a>
+                        
+
+                    </div>
+                </form>            
+            </div>
+
+            
         </div>
-
-        <div class="col-6">
-            <?php require_once(__DIR__ . "/../include/msg.php"); ?>
-        </div>
-    </div>
-
+</div>
+        
+            <!-- <div class="col-6">
+                <?php require_once(__DIR__ . "/../include/msg.php"); ?>
+            </div> -->
 <script>
     const selectFuncao = document.getElementById('selfuncao');
     const matriculaGroup = document.getElementById('matricula-group');
--- app/view/login/login.php
+++ app/view/login/login.php
@@ -5,43 +5,45 @@ $pagina = 'login';
 
 require_once(__DIR__ . "/../include/header.php");
 ?>
-<div class="container d-flex justify-content-center" style="padding-top: 80px;">
-    <div class="row w-100" style="max-width: 1000px;">
+<div class="container d-flex justify-content-center" style="padding-top: 50px; max-height: 600px;">
+    <div class="row w-100" style="max-width: 800px;">
         <!-- CARD DO FORMULÁRIO -->
         <div class="col-md-6">
-            <div class="card shadow p-4 rounded" style="width: 700px; background-color: #f8f9fa;">
+           <div class="card shadow p-1 rounded-lg" style="width: 700px; height: 750px; background-color: rgba(255, 255, 255, 0.33); backdrop-filter: blur(10px); -webkit-backdrop-filter: blur(10px); border: 1px solid rgba(255,255,255,0.3);">
+                <img src="<?= BASEURL . '/view/img/anexo.png'?>" style="display: block; margin: 0 auto 10px auto; max-width: 500px%; height: 300px;">
 
-                <img src="<?= BASEURL . '/view/img/anexo.png'?>" style="display: block; margin: 0 auto 10px auto; max-width: 500px%; height: auto;">
-
-                <h4 style="text-align: center;">login:</h4>
-
+                <h3 class="mb-3"  style="text-align: center;"><B>LOGIN</B></h3>
+                <!-- MENSAGENS DE ERRO AO LADO -->
+                <div class=" align-items-center" style="max-width: 600px; padding-left: 85px ;">
+                    <h11  style="text-align: center;"><?php include_once(__DIR__ . "/../include/msg.php") ?></h11>
+                </div>
                 <form id="frmLogin" action="./LoginController.php?action=logon" method="POST">
-                    <div class="mb-3">
-                        <label class="form-label" for="txtEmail">Login:</label>
+                    <div class="mb-3" style="max-width: 600px; padding-left: 85px ;" >
+                        <!--<label class="form-label" for="txtEmail">Login:</label>-->
                         <input type="text" class="form-control" name="email" id="txtEmail"
-                               maxlength="15" placeholder="Informe o email"
+                               maxlength="15" placeholder="Email"
                                value="<?php echo isset($dados['email']) ? $dados['email'] : '' ?>" />        
                     </div>
 
-                    <div class="mb-3">
-                        <label class="form-label" for="txtSenha">Senha:</label>
+                    <div class="mb-3" style="max-width: 600px; padding-left: 85px ;">
+                        <!--<label class="form-label" for="txtSenha">Senha:</label>-->
                         <input type="password" class="form-control" name="senha" id="txtSenha"
-                               maxlength="15" placeholder="Informe a senha"
+                               maxlength="15" placeholder="Senha"
                                value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>" />        
                     </div>
+                    
+                    <div class="mb-3" style="padding-left: 85px;">
 
-                    <button type="submit" class="btn btn-success mt-3">Acessar</button>
-                    <button class="btn btn-success mt-3">
-                        <a href="<?= BASEURL . '/controller/CadastroController.php?action=create' ?>" style="color: white; text-decoration: none;">Cadastrar</a>
-                    </button>
+                        <button type="submit" class="btn btn-success mt-3">Acessar</button>
+                        <a href="<?= BASEURL . '/controller/CadastroController.php?action=create' ?>" class="btn btn-primary mt-3">Cadastrar</a>
+                        </div>
+                        
+                    <h6 class="mb-4" style="padding-left: 85px; padding-top: 30px">Bora construi um futuro melhor 💪🎓📚</h6>
                 </form>
+                
             </div>
         </div>
 
-        <!-- MENSAGENS DE ERRO AO LADO -->
-        <div class="col-md-6 d-flex align-items-center">
-            <?php include_once(__DIR__ . "/../include/msg.php") ?>
-        </div>
     </div>
 </div>
 
